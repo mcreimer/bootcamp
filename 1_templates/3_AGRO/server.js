7cc4b624b9f50cadd8bafdf55524212b61b45757
Marcelo Creimer <14227072+mcreimer@users.noreply.github.com>
2017-12-22 11:35:23 -0200

    Delete server.js

--- 1_templates/3_AGRO/server.js
+++
@@ -1,5 +0,0 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic('C:/Users/AdminDLT/Desktop/Bootcamp/examples/template')).listen(8080, function(){
-    console.log('Server running on 8080...');
-});
